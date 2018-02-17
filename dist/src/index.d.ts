0735bbb371b9653e3de11ceb8b4abcd16955e6a5
Dennis Teo <dennis@anel.co>
2018-02-17 15:49:49 +0100

    externals and test fixes

--- dist/src/index.d.ts
+++
@@ -1,2 +0,0 @@
-import { DynaFieldWrapper, IDynaFieldWrapperProps, TContent, EMode, EStyle, EColor } from "./DynaFieldWrapper";
-export { DynaFieldWrapper, IDynaFieldWrapperProps, TContent, EMode, EStyle, EColor };
