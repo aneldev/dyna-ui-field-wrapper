05685023393b7458363bc5eb355307ed354d2f8a
Dennis Teo <dynasdaq@gmail.com>
2018-02-17 12:28:30 +0100

    animations

---
+++ dist/src/index.d.ts
@@ -0,0 +1,2 @@
+import { DynaFieldWrapper, IDynaFieldWrapperProps, TContent, EMode, EStyle, EColor } from "./DynaFieldWrapper";
+export { DynaFieldWrapper, IDynaFieldWrapperProps, TContent, EMode, EStyle, EColor };
